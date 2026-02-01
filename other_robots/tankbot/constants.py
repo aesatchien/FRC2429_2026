4eca8edf890ab8571117d07c60254b241e81dd73
workshop <workshop@example.com>
2026-01-31 18:42:45 -0800

    update tankbot for spark flex

--- other_robots/tankbot/constants.py
+++ other_robots/tankbot/constants.py
@@ -9,24 +9,8 @@ import math  # use this for pi and tau, cos and sin if necessary
 import rev
 from rev import SparkMaxConfig, SparkFlexConfig  # i think we can do SparkBaseConfig - it works for Max and Flex controllers
 from typing import Union, List
+from helpers.utilities import set_config_defaults
 
-
- # ----------------  COMMON FUNCTIONS  ------------------------------------
-def set_config_defaults(configs: Union[SparkMaxConfig, List[SparkMaxConfig]]) -> None:
-    """
-    Applies default configuration settings to a single config object or a list of config objects.
-    Args:
-        configs: A single configuration object or a list of configuration objects.
-    """
-    # Check if the input is a list (or any sequence except a string/bytes)
-    if isinstance(configs, (list, tuple)):
-        config_list = configs
-    else:
-        config_list = [configs]  # If it's a single item, wrap it in a list for the loop
-    for config in config_list:
-        config.voltageCompensation(12)
-        config.setIdleMode(SparkMaxConfig.IdleMode.kBrake)
-        config.smartCurrentLimit(40)
 
 # general constants
 k_burn_flash = True  # whether to burn the configurations into the spark maxes
@@ -98,7 +82,7 @@ class ShooterConstants:
     if k_control_type == 'max_motion':
         # maxmotion - allows us to set mav velocity, acceleration and jerk, letting us crank proportional response
         motor_max_rpm = 6784  # neo vortex
-        k_flywheel_left_leader_config.closedLoop.pidf(p=1e-4, i=0, d=0, ff=1 / motor_max_rpm, slot=rev.ClosedLoopSlot.kSlot0)
+        k_flywheel_left_leader_config.closedLoop.pidf(p=2e-4, i=0, d=0, ff=1 / motor_max_rpm, slot=rev.ClosedLoopSlot.kSlot0)
 
         # Configure MAXMotion (The "Modern" Smart Motion) - Note: "maxMotion" object instead of "smartMotion"
         k_flywheel_left_leader_config.closedLoop.maxMotion.cruiseVelocity(6000, slot=rev.ClosedLoopSlot.kSlot0)
--- other_robots/tankbot/subsystems/shooter.py
+++ other_robots/tankbot/subsystems/shooter.py
@@ -13,18 +13,18 @@ class Shooter(Subsystem):
     def __init__(self) -> None:
         super().__init__()
         self.setName('Shooter')
-        self.allowed_rpms = [i for i in range(0, 5001, 200) if i >= 2600 or i == 0] # gives valid rpms from 2600 to 5000 increments of 200
+        self.allowed_rpms = [0, 60] + [i for i in range(2000, 5001, 250)]  # CJH putting in other test RPMS
         self.current_index = 4  # start at 4000 rpm
         self.counter = sc.k_flywheel_counter_offset  # note this should be an offset in constants
         self.current_rpm = sc.k_test_rpm
 
         # --------------- add motors and shooter rpm ----------------
         
-        motor_type = rev.SparkMax.MotorType.kBrushless
-        self.flywheel_left_leader = rev.SparkMax(sc.k_CANID_flywheel_left_leader, motor_type)
-        self.flywheel_right_follower = rev.SparkMax(sc.k_CANID_flywheel_right_follower, motor_type)
-        self.flywheel_back_follower = rev.SparkMax(sc.k_CANID_flywheel_back_follower, motor_type)
-        self.indexer_motor = rev.SparkMax(sc.k_CANID_indexer, motor_type)
+        motor_type = rev.SparkFlex.MotorType.kBrushless
+        self.flywheel_left_leader = rev.SparkFlex(sc.k_CANID_flywheel_left_leader, motor_type)
+        self.flywheel_right_follower = rev.SparkFlex(sc.k_CANID_flywheel_right_follower, motor_type)
+        self.flywheel_back_follower = rev.SparkFlex(sc.k_CANID_flywheel_back_follower, motor_type)
+        self.indexer_motor = rev.SparkMax(sc.k_CANID_indexer, rev.SparkMax.MotorType.kBrushless)
 
         # convenient list of motors if we need to query or set all of them
         self.motors = [self.flywheel_left_leader, self.flywheel_right_follower, self.indexer_motor]
@@ -83,15 +83,17 @@ class Shooter(Subsystem):
         # multiple different ways to set the shooter - simplest is self.flywheel_left_leader.set(rpm)
 
         if sc.k_control_type == 'max_motion':
+            ks = 0 if rpm < 1 else sc.ks_volts  # otherwise it still just turns at 0
             self.flywheel_controller.setSetpoint(setpoint=rpm, ctrl=SparkLowLevel.ControlType.kMAXMotionVelocityControl,
-                                                 slot=rev.ClosedLoopSlot.kSlot0, arbFeedforward=sc.ks_volts)
+                                                 slot=rev.ClosedLoopSlot.kSlot0, arbFeedforward=ks)
         else:
             feed_forward = min(12.0, 12.0 * rpm / sc.motor_max_rpm)  # if there is no gearing, then this gets you close
             # rev is a pain in the ass - you have to pass EXACTLY the types it wants - no using "0" for the slots anymore
             self.flywheel_controller.setSetpoint(setpoint=rpm, ctrl=SparkLowLevel.ControlType.kVelocity, slot=rev.ClosedLoopSlot.kSlot0, arbFeedforward=feed_forward)
+            self.voltage = feed_forward  # 12 * rpm / max rpm  # Guess
         print(f'set flywheel rpm to {rpm:.0f}')  # want to say what time it is, but can't import the container's timer easily
         self.shooter_on = True
-        self.voltage = feed_forward  # 12 * rpm / max rpm  # Guess
+
         SmartDashboard.putBoolean('shooter_on', self.shooter_on)  # ideally these will be publishers later
         SmartDashboard.putNumber('shooter_rpm', self.current_rpm)
 
@@ -142,104 +144,3 @@ class Shooter(Subsystem):
 
 
 
-#2024 shooter code
-#
-# class Shooter(Subsystem):
-#     def __init__(self):
-#         super().__init__()
-#         self.setName('Shooter')
-#         self.counter = 0
-#         # self.PID_dict_vel = {'kP': 0.00021, 'kI': 0, 'kD': 0, 'kIz': 0, 'kFF': 0.000192}
-#         self.smartmotion_maxvel = 5001  # rpm
-#         self.smartmotion_maxacc = 5001
-#         self.current_limit = 35
-#         self.voltage = 5
-#         self.rpm = 1000
-#         self.rpm_list = [1000, 2000, 3000, 4000, 5000, 4000, 3000, 2000]  # cycle through these
-#         self.rpm_index = 0
-#
-#         # initialize motors
-#         # looking from back to front
-#         motor_type = rev.CANSparkFlex.MotorType.kBrushless
-#         self.flywheel_lower_left = rev.CANSparkFlex(constants.k_flywheel_lower_left_neo_port, motor_type)
-#         self.flywheel_upper_left = rev.CANSparkFlex(constants.k_flywheel_upper_left_neo_port, motor_type)
-#
-#         # the follower is inverted
-#         self.flywheel_lower_left.setInverted(False)
-#         self.flywheel_upper_left.setInverted(True)
-#
-#         # encoders
-#         self.flywheel_left_encoder = self.flywheel_lower_left.getEncoder()
-#
-#         # controller
-#         self.flywheel_lower_left_controller = self.flywheel_lower_left.getPIDController()
-#         self.flywheel_lower_left_controller.setP(0.0001)
-#         self.flywheel_upper_left_controller = self.flywheel_upper_left.getPIDController()
-#         self.flywheel_upper_left_controller.setP(0.0001)
-#         self.kFF = 1.03 * 1 / 6784  # feed forward for a spark flex shooter
-#         self.flywheel_lower_left_controller.setFF(self.kFF, 0)
-#         self.flywheel_upper_left_controller.setFF(self.kFF, 0)
-#
-#         # toggle state
-#         self.shooter_on = False
-#         SmartDashboard.putBoolean('shooter_on', self.shooter_on)
-#
-#         # TODO configure_sparkmax()
-#         # configure_sparkmax()
-#
-#     def set_flywheel(self, rpm):
-#         # self.flywheel_left_controller.setReference(rpm, rev.CANSparkLowLevel.ControlType.kSmartVelocity, 0)
-#         # self.shooter_voltage = self.shooter_voltage + 1 if self.shooter_voltage < 12 else 5  # CJH increment voltage test
-#         # self.shooter_rpm = self.shooter_rpm + 1000 if self.shooter_rpm < 5000 else 1000  # AEH increment rpm test
-#
-#         if rpm is None:  # cycle through for testing or actually pass a value
-#             self.rpm_index += 1
-#             self.rpm = self.rpm_list[self.rpm_index % len(self.rpm_list)]
-#         else:
-#             self.rpm = rpm
-#
-#         use_voltage = False
-#         if use_voltage:
-#             self.flywheel_lower_left_controller.setReference(self.voltage, rev.CANSparkFlex.ControlType.kVoltage, 0)
-#             self.flywheel_upper_left_controller.setReference(self.voltage, rev.CANSparkFlex.ControlType.kVoltage, 0)
-#         else:
-#             self.flywheel_lower_left_controller.setReference(self.rpm, rev.CANSparkFlex.ControlType.kVelocity, 0)
-#             self.flywheel_upper_left_controller.setReference(self.rpm, rev.CANSparkFlex.ControlType.kVelocity, 0)
-#         # self.flywheel_left_controller.setReference(self.shooter_voltage, rev.CANSparkFlex.ControlType.kVoltage, 0)
-#         # self.flywheel_right_controller.setReference(self.shooter_voltage, rev.CANSparkLowLevel.ControlType.kVoltage, 0)
-#         self.shooter_on = True
-#         print(f'setting rpm to {rpm} {self.voltage}')
-#         SmartDashboard.putBoolean('shooter_on', self.shooter_on)
-#
-#     def stop_shooter(self):
-#         self.flywheel_lower_left_controller.setReference(0, rev.CANSparkFlex.ControlType.kVoltage)
-#         self.flywheel_upper_left_controller.setReference(0, rev.CANSparkFlex.ControlType.kVoltage)
-#         # self.flywheel_left_controller.setReference(0, rev.CANSparkFlex.ControlType.kVoltage)
-#         # self.flywheel_right_controller.setReference(0, rev.CANSparkLowLevel.ControlType.kVoltage)
-#         self.shooter_on = False
-#         self.voltage = 0  # CJH for 2024 testing
-#         SmartDashboard.putBoolean('shooter_on', self.shooter_on)
-#
-#     def get_velocity(self):
-#         return self.flywheel_left_encoder.getVelocity()
-#
-#     def toggle_shooter(self, rpm):
-#         if self.shooter_on:
-#             self.stop_shooter()
-#         else:
-#             self.rpm = self.rpm if rpm is None else rpm
-#             self.set_flywheel(self.rpm)
-#
-#     def periodic(self) -> None:
-#
-#         self.counter += 1
-#
-#         # SmartDashboard.putBoolean('shooter_enable', self.shooter_enable)
-#         if self.counter % 20 == 0:
-#             # not too often
-#             SmartDashboard.putNumber('shooter_rpm', self.flywheel_left_encoder.getVelocity())
-#             SmartDashboard.putNumber('shooter_rpm_target', self.rpm)
-#             at_velocity = math.fabs(self.get_velocity() - self.rpm) < 200  # need to figure out this tolerance
-#             SmartDashboard.putBoolean('shooter_ready', at_velocity)
-#             SmartDashboard.putNumber('shooter_current', self.flywheel_lower_left.getOutputCurrent())
-#             SmartDashboard.putNumber('shooter_output', self.flywheel_lower_left.getAppliedOutput())
