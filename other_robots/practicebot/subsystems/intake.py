a48a9dc3d08a3e1274465f5753a6cd72f889c0a9
Trent24D <trentandave1@gmail.com>
2026-02-04 10:12:51 -0800

    adding a dropper motor and function TODO later

--- other_robots/practicebot/subsystems/intake.py
+++ other_robots/practicebot/subsystems/intake.py
@@ -20,13 +20,16 @@ class Intake(Subsystem):
         
         motor_type = rev.SparkMax.MotorType.kBrushless
         self.intake = rev.SparkMax(ic.k_CANID_intake, motor_type)
+        self.dropper = rev.SparkMax(ic.k_CANID_dropper, motor_type)
 
         # convenient list of motors if we need to query or set all of them
-        self.motors = [self.intake]
+        self.motors = [self.intake, self.dropper]
 
         # you need a controller to set velocity
         self.intake_controller = self.intake.getClosedLoopController()
         self.intake_encoder = self.intake.getEncoder()
+        self.dropper_controller = self.dropper.getClosedLoopController()
+        self.dropper_encoder = self.dropper.getEncoder()
 
         # default parameters for the sparkmaxes reset and persist modes -
         self.rev_resets = rev.ResetMode.kResetSafeParameters
@@ -34,7 +37,7 @@ class Intake(Subsystem):
             else rev.PersistMode.kNoPersistParameters
 
         # put the configs in a list matching the motors
-        self.configs = ic.k_intake_configs
+        self.configs = [ic.k_intake_configs, ic.k_dropper_configs]
 
         # this should be its own function later - we will call it whenever we change brake mode
         rev_errors = [motor.configure(config, self.rev_resets, self.rev_persists)
@@ -42,16 +45,21 @@ class Intake(Subsystem):
 
         # initialize states
         self.intake_on = False
+        self.dropper_down = False
         self.current_rpm = 0
         self._init_networktables()
 
     def _init_networktables(self):
         self.inst = ntcore.NetworkTableInstance.getDefault()
+
         self.intake_prefix = constants.intake_prefix
         self.intake_on_pub = self.inst.getBooleanTopic(f"{self.intake_prefix}/intake_on").publish()
         self.intake_rpm_pub = self.inst.getDoubleTopic(f"{self.intake_prefix}/intake_rpm").publish()
+        self.dropper_down_pub = self.inst.getBooleanTopic(f"{self.intake_prefix}/dropper_down").publish()
+        
         self.intake_on_pub.set(self.intake_on)
         self.intake_rpm_pub.set(self.current_rpm)
+        self.dropper_down_pub.set(self.dropper_down)
 
     def stop_intake(self):
         # three different ways to stop the intake
@@ -61,21 +69,31 @@ class Intake(Subsystem):
 
         self.intake_on = False
         self.current_rpm = 0
+        self.dropper_down = False
         self.intake_on_pub.set(self.intake_on)
         self.intake_rpm_pub.set(self.current_rpm)
+        self.dropper_down_pub.set(self.dropper_down)
 
     def set_intake_rpm(self, rpm=1000):
         # TODO - incorporate a PID to handle voltage sag from multiple balls
         feed_forward = min(12, 12 * rpm / 5600)  # if there is no gearing, then this gets you close
+        self.set_dropper_down(down=True) if self.dropper_down == False else None
         self.intake_controller.setReference(setpoint=rpm, ctrl=SparkLowLevel.ControlType.kVelocity, slot=rev.ClosedLoopSlot.kSlot0, arbFeedforward=feed_forward)
         print(f'set intake rpm to {rpm:.0f}')  # want to say what time it is, but can't import the container's timer easily - could use the wpilib timer
         self.intake_on = True
         self.current_rpm = rpm
+
+
         self.intake_on_pub.set(self.intake_on)
         self.intake_rpm_pub.set(self.current_rpm)
+        self.dropper_down_pub.set(self.dropper_down)
 
     def get_rpm(self):
         return self.intake_encoder.getVelocity()
+
+    def set_dropper_down(self, down=True):
+        # function that moves intake down to the ground, or up to stow it
+        pass
 
     def toggle_intake(self, rpm):
         if self.intake_on:
