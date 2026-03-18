b4639ba25cf72053d405f1a3566031704519be9f
aesatchien <cjh123@yahoo.com>
2026-03-17 22:04:06 -0700

    updating gui LCDs

--- gui/config.py
+++ gui/config.py
@@ -158,12 +158,12 @@ WIDGET_CONFIG = {
 
     # NUMERIC INDICATORS - I HAVE BEEN USING THE LCD FOR THIS BUT THERE BUST BE A BETTER WAY TO SHOW NUMBERS
     # TODO - write a pretty custom indicator for these numbers using a robot font, black background, and red numbers
-    'qlcd_navx_heading': {'widget_name': 'qlcd_navx_heading', 'nt_topic': '/SmartDashboard/_navx', 'update_style': 'lcd'},
-    'qlcd_elevator_height': {'widget_name': 'qlcd_elevator_height', 'nt_topic': '/SmartDashboard/elevator_spark_pos', 'update_style': 'lcd'},
-    'qlcd_pivot_angle': {'widget_name': 'qlcd_pivot_angle', 'nt_topic': '/SmartDashboard/profiled_pivot_spark_angle', 'update_style': 'lcd'},
-    'qlcd_wrist_angle': {'widget_name' :'qlcd_wrist_angle', 'nt_topic': '/SmartDashboard/wrist relative encoder, degrees', 'update_style': 'lcd'},
-    'qlcd_intake_speed': {'widget_name': 'qlcd_intake_speed', 'nt_topic': '/SmartDashboard/intake_output', 'update_style': 'lcd'},
-    'qlcd_climber_position': {'widget_name': 'qlcd_climber_position', 'nt_topic': '/SmartDashboard/climber_spark_angle', 'update_style': 'lcd'},
+    'qlcd_navx_heading': {'widget_name': 'qlcd_navx_heading', 'nt_topic': f'{base_prefix}/_navx', 'update_style': 'lcd'},
+    'qlcd_intake_speed': {'widget_name': 'qlcd_intake_speed', 'nt_topic': f'{base_prefix}/Intake/intake_rpm', 'update_style': 'lcd'},
+    'qlcd_intake_position': {'widget_name': 'qlcd_intake_position', 'nt_topic': f'{base_prefix}/Intake/deploy_angle', 'update_style': 'lcd'},
+    'qlcd_hopper_speed': {'widget_name': 'qlcd_hopper_speed', 'nt_topic': f'{base_prefix}/Shooter/hopper_rpm', 'update_style': 'lcd'},
+    'qlcd_indexer_speed': {'widget_name': 'qlcd_indexer_speed', 'nt_topic': f'{base_prefix}/Shooter/indexer_rpm', 'update_style': 'lcd'},
+    'qlcd_shooter_speed': {'widget_name' :'qlcd_shooter_speed', 'nt_topic': f'{base_prefix}/Shooter/measured_shooter_rpm', 'update_style': 'lcd'},
 
     # REBUILT 2026 stuff
     'qlabel_shot_distance': {'widget_name': 'qlabel_shot_distance', 'nt_topic': f'{auto_prefix}/shot_distance', 'update_style': 'hub'},
