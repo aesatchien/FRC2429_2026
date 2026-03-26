e97e8eca0071d398487efdadf74695e8bf82ecad
aesatchien <cjh123@yahoo.com>
2026-03-25 21:32:01 -0700

    update model for lower intake gearing

--- comp_bot/constants.py
+++ comp_bot/constants.py
@@ -223,9 +223,9 @@ class IntakeConstants:
     k_max_velocity_rad_per_second = math.pi
     k_max_acceleration_rad_per_sec_squared = math.pi
     k_kS_volts = 0.01  # constant to always add, uses the sign of velocity
-    k_kG_volts = 0.56 / 1.0  # 10lbs at 11in COM, cuts in half with two motors, goes up with mass and distance, down with efficiency
-    k_kV_volt_second_per_radian = 4.75  # stays the same with one or two motors, based on the NEO vortex itself and gear ratio
-    k_kA_volt_second_squared_per_meter = 0.10 / 1.0  # cuts in half with 2 motors, but we only use one
+    k_kG_volts = 1.03 / 1.0  # 10lbs at 11in COM, cuts in half with two motors, goes up with mass and distance, down with efficiency
+    k_kV_volt_second_per_radian = 2.43  # stays the same with one or two motors, based on the NEO vortex itself and gear ratio
+    k_kA_volt_second_squared_per_meter = 0.21 / 1.0  # cuts in half with 2 motors, but we only use one
     k_starting_angle = math.radians(k_bottom_angle) if k_at_home else math.radians(k_top_angle)
 
 class ShooterConstants:
