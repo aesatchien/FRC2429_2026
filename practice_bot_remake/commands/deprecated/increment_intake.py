093d50b314e615286dac1d905572669516828b9d
Trent24D <trentandave1@gmail.com>
2026-04-22 14:08:53 -0700

    practice_bot new code

---
+++ practice_bot_remake/commands/deprecated/increment_intake.py
@@ -0,0 +1,37 @@
+import commands2
+from helpers.log_command import log_command  # outsource explicit logging clutter to a single line
+
+from constants import IntakeConstants as ic
+from subsystems.intake import Intake
+
+
+@log_command(console=True, nt=False, print_init=True, print_end=True)  # will print start and end messages
+class IncrementIntake(commands2.Command):  # change the name for your command
+
+    def __init__(self, intake: Intake, indent=0, speed_change=0) -> None:
+        super().__init__()
+        self.setName('Increment Intake')
+        self.indent = indent
+        self.intake = intake
+        self.speed_change = speed_change
+        self.addRequirements(self.intake)  # commandsv2 version of requirements
+        self.extra_log_info = None
+
+    def initialize(self) -> None:
+        # Called just before each time this Command runs
+        # if you wish to add more information to the console logger, change self.extra_log_info
+        # self.extra_log_info = "Target=7"  # (for example)
+        self.intake.change_speed(self.speed_change)
+        self.intake.set_intake_rpm(self.intake.default_rpm)
+
+    def execute(self) -> None:
+        # runs 50x per second, so be careful about messages and timing
+        pass
+
+    def isFinished(self) -> bool:
+        # True: fire once and end; False: run forever until interrupted; logic has it end when code returns True
+        return True
+
+    def end(self, interrupted: bool) -> None:
+        # put your safe cleanup code here - turn off motors, set LEDs, etc
+        pass
---
+++ practice_bot_remake/commands/deprecated/stop_shooting.py
@@ -0,0 +1,49 @@
+import commands2
+from helpers.log_command import log_command  # outsource explicit logging clutter to a single line
+
+from constants import ShooterConstants as sc
+from subsystems.shooter import Shooter
+
+@log_command(console=True, nt=False, print_init=True, print_end=True)  # will print start and end messages
+class ShootingCommand(commands2.Command):  # change the name for your command
+
+
+    def __init__(self, shooter: Shooter, indent=0) -> None:
+        super().__init__()
+        self.setName('Shooting') # change this to something appropriate for this command
+        self.indent = indent
+        self.shooter = shooter
+        self.addRequirements(self.shooter)  # commandsv2 version of requirements
+        self.extra_log_info = None
+        self.counter = 0  # add a counter if you need to track iterations, remember to initialize in below
+        # we want indexer and hopper to start after .1 seconds or 1/10 seconds. 
+        # if it runs 50x per second, 50 * 1/10 is 5, so after 5 cycles, start the indexer and hopper
+        self.delay_cycles = 5
+
+    def initialize(self) -> None:
+        # Called just before each time this Command runs
+        # if you wish to add more information to the console logger, change self.extra_log_info
+        # self.extra_log_info = "Target=7"  # (for example)
+        self.counter = 0
+        # self.shooter.set_shooter_rpm(sc.k_shooter_test_speed)
+        # self.shooter.set_indexer_rpm(sc.k_indexer_rpm)
+        # self.shooter.set_hopper_rpm(sc.k_hopper_rpm)
+        self.shooter.set_shooter_rpm(5000)
+        self.shooter.set_indexer_rpm(sc.k_indexer_rpm)
+        self.shooter.set_hopper_rpm(sc.k_hopper_rpm)
+
+
+    def execute(self) -> None:
+        self.counter += 1
+        # if self.counter > self.delay_cycles and (self.shooter.indexer_on == False or self.shooter.hopper_on == False):
+        #     self.shooter.set_indexer_rpm(sc.k_indexer_rpm)
+        #     self.shooter.set_hopper_rpm(sc.k_hopper_rpm)
+        # runs 50x per second, so be careful about messages and timing
+
+    def isFinished(self) -> bool:
+        # True: fire once and end; False: run forever until interrupted; logic has it end when code returns True
+        return False
+        
+    def end(self, interrupted: bool) -> None:
+        # put your safe cleanup code here - turn off motors, set LEDs, etc
+        self.shooter.stop_shooter()
